26687284a0c5fa0f7d6cb11dd9a46dd977e70f91
Souza032 <gustavo.souza1205@gmail.com>
2023-03-29 09:25:09 -0300

    j

---
+++ src/br/com/dio/primeiro_programa.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiro_programa {
+    public static void main(String[] args) {
+
+        gato gato = new gato("bil", "azul", 3);
+
+
+        System.out.println(gato);
+    }
+}
